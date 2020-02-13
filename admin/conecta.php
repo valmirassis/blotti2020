0ea1421be635772d210342a4b32b66d82c4a5ecf
Assis <assis@rediscover.com.br>
2020-02-13 18:23:43 -0300

    add admin

---
+++ admin/conecta.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+$servidor   =   "localhost";   //SERVIDOR
+$bd         =   "appb";       //DATABASE
+$usuario    =   "root";        //USU�RIO
+$senha      =   "";            //SENHA
+
+$link = mysqli_connect($servidor, $usuario, $senha, $bd);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+// echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+// echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+?>
---
+++ admin/home.php
@@ -0,0 +1,68 @@
+    <?php
+  require_once("verifica.php");
+    include ('conecta.php');
+  $cod_usuario = $_SESSION['cod'];
+    ?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Ambiente Restrito: ... Kffk Empreendimentos...</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <header>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <!-- Brand -->
+        <!-- <a class="navbar-brand" href="#"><img src="img/blotti_movimentacao_de_cargas_acessorios.png" width="150px"></a> -->
+      
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button> 
+      
+        <!-- Navbar links -->
+       <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link anima" href="#home"><i class="fas fa-home"></i> Produtos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link anima" href="#"><i class="fas fa-industry"></i> Empresa </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link anima" href="#"><i class="fas fa-archive"></i> Layout</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link anima" href="#"><i class="fas fa-cogs"></i> Vídeos</a>
+              </li>             
+                <li class="nav-item">
+                        <a class="nav-link anima" href="#contato"><i class="fas fa-envelope"></i> Contato</a>
+                </li>    
+          </ul>
+        </div> 
+      </nav> 
+ 
+    </header>
+   
+    <section class="content">
+<div class="container">
+    <div class="row">
+        <div class="col">
+    <!-- <h1>Inspeções cadastradas</h1> -->
+ 
+    </div>
+    </div>
+</div>
+</section>
+
+    </body>
+    </html>
